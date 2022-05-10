5051367d2f4f5b73298092b6abf5fbf259fadbfd
Pedro Palmares <petipalmares@gmail.com>
2022-05-10 23:34:40 +0100

    added photos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'black': '#242323',
+        'grey': '#737373',
+        'lightgrey': '#d4d4d4'
+        // 'green': '#00B805',
+        // 'darkgreen': '#008E04',
+        // 'purple': '#6A67E2',
+        // 'pink': '#F15484',
+        // 'blue': '#78CDEE'
+      },
+      fontFamily: {
+        'sans': ['Montserrat', ...defaultTheme.fontFamily.sans],
+      },
+    }
   },
   plugins: [],
 }
