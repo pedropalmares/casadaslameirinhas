c39f1e7359de0fb9d481208147bf04f62dc8df41
Pedro Palmares <petipalmares@gmail.com>
2023-08-06 18:23:03 +0100

    novo design

--- site.js
+++ site.js
@@ -1,12 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var lazyloadImages = document.querySelectorAll("img.lazy");
+  var lazyloadImages = document.querySelectorAll("img.lazy");
 
-    function lazyload() {
-        lazyloadImages.forEach(function (img) {
-            img.src = img.dataset.src;
-            img.classList.remove('lazy');
-        });
-    }
+  function lazyload() {
+    lazyloadImages.forEach(function (img) {
+      img.src = img.dataset.src;
+      img.classList.remove("lazy");
+    });
+  }
 
-    window.addEventListener("load", lazyload);
+  window.addEventListener("load", lazyload);
+});
+
+$(".section_button").click(function () {
+  $("section").hide();
+  $("#" + this.id.replace("_button", "")).show();
 });
